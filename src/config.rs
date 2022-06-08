223f2611b05f7ad516b5336e7e6f915e919996cb
Vladislav Mamon <norskeld@gmail.com>
2022-06-08 16:02:56 +0500

    feat: basic config parsing w/ KDL

---
+++ src/config.rs
@@ -0,0 +1,155 @@
+use kdl::{KdlDocument, KdlEntry, KdlNode};
+
+#[derive(Debug)]
+pub struct Replacement {
+  pub tag: String,
+  pub description: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum Action {
+  Suite(Vec<ActionSuite>),
+  Single(Vec<ActionSingle>),
+}
+
+#[derive(Debug)]
+pub struct ActionSuite {
+  pub name: String,
+  pub actions: Vec<ActionSingle>,
+  pub requirements: Vec<String>,
+}
+
+#[derive(Debug)]
+pub enum ActionSingle {
+  Copy {
+    from: Option<String>,
+    to: Option<String>,
+    overwrite: bool,
+  },
+  Move {
+    from: Option<String>,
+    to: Option<String>,
+    overwrite: bool,
+  },
+  Delete {
+    target: Option<String>,
+  },
+  Run {
+    command: Option<String>,
+  },
+  Unknown,
+}
+
+pub fn get_actions(doc: &KdlDocument) -> Option<Action> {
+  doc
+    .get("actions")
+    .and_then(|node| node.children())
+    .map(|children| {
+      let nodes = children.nodes();
+
+      if nodes.iter().all(is_suite) {
+        let suites = nodes.iter().filter_map(to_sequence).collect::<Vec<_>>();
+        Action::Suite(suites)
+      } else {
+        let operations = nodes.iter().filter_map(to_operation).collect::<Vec<_>>();
+        Action::Single(operations)
+      }
+    })
+}
+
+pub fn get_replacements(doc: &KdlDocument) -> Option<Vec<Replacement>> {
+  doc
+    .get("replacements")
+    .and_then(|node| node.children())
+    .map(|children| {
+      children
+        .nodes()
+        .iter()
+        .filter_map(to_replacement)
+        .collect::<Vec<_>>()
+    })
+}
+
+fn to_replacement(node: &KdlNode) -> Option<Replacement> {
+  let tag = node.name().to_string();
+  let description = node.get(0).map(entry_to_string);
+
+  Some(Replacement { tag, description })
+}
+
+fn to_operation(node: &KdlNode) -> Option<ActionSingle> {
+  let action = to_action(node);
+
+  if let ActionSingle::Unknown = action {
+    None
+  } else {
+    Some(action)
+  }
+}
+
+fn to_sequence(node: &KdlNode) -> Option<ActionSuite> {
+  let name = node.get("name").map(entry_to_string);
+  let requirements = node.get("requires").map(entry_to_string).map(|value| {
+    value
+      .split_ascii_whitespace()
+      .map(str::to_string)
+      .collect::<Vec<String>>()
+  });
+
+  let actions = node
+    .children()
+    .map(|children| children.nodes().iter().map(to_action).collect::<Vec<_>>());
+
+  let suite = (
+    name,
+    actions.unwrap_or(vec![]),
+    requirements.unwrap_or(vec![]),
+  );
+
+  match suite {
+    | (Some(name), actions, requirements) => Some(ActionSuite {
+      name,
+      actions,
+      requirements,
+    }),
+    | _ => None,
+  }
+}
+
+fn to_action(node: &KdlNode) -> ActionSingle {
+  let action_kind = node.name().to_string();
+
+  match action_kind.to_ascii_lowercase().as_str() {
+    | "copy" => ActionSingle::Copy {
+      from: node.get("from").map(entry_to_string),
+      to: node.get("to").map(entry_to_string),
+      overwrite: node
+        .get("overwrite")
+        .and_then(|value| value.value().as_bool())
+        .unwrap_or(true),
+    },
+    | "move" => ActionSingle::Move {
+      from: node.get("from").map(entry_to_string),
+      to: node.get("to").map(entry_to_string),
+      overwrite: node
+        .get("overwrite")
+        .and_then(|value| value.value().as_bool())
+        .unwrap_or(true),
+    },
+    | "delete" => ActionSingle::Delete {
+      target: node.get(0).map(entry_to_string),
+    },
+    | "run" => ActionSingle::Run {
+      command: node.get(0).map(entry_to_string),
+    },
+    | _ => ActionSingle::Unknown,
+  }
+}
+
+fn is_suite(node: &KdlNode) -> bool {
+  node.name().value().to_string().eq("suite".into())
+}
+
+fn entry_to_string(entry: &KdlEntry) -> String {
+  entry.value().to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-use imp::app;
+use std::env;
+use std::fs;
 
-fn main() {
-  app::run()
+use kdl::KdlDocument;
+use imp::config;
+
+// use imp::app::{self, AppError};
+
+// #[tokio::main]
+// async fn main() -> Result<(), AppError> {
+//   app::run().await
+// }
+
+fn main() -> std::io::Result<()> {
+  let mut filename = env::current_dir()?;
+  filename.push("imp.kdl");
+
+  let contents = fs::read_to_string(filename)?;
+  let doc: KdlDocument = contents.parse().expect("Failed to parse config file.");
+
+  println!("{:#?}", config::get_replacements(&doc));
+  println!("{:#?}", config::get_actions(&doc));
+
+  Ok(())
 }

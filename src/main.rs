3072f76af05f61fecdba4dbca1275d4244efe5e2
Vladislav Mamon <norskeld@gmail.com>
2022-08-13 20:27:16 +0500

    style(main): remove commented out code

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,3 @@ use arx::app::{self, AppError};
 async fn main() -> Result<(), AppError> {
   app::run().await
 }
-
-// use std::env;
-// use std::fs;
-
-// use kdl::KdlDocument;
-// use imp::config::{self, Action};
-
-// fn main() -> std::io::Result<()> {
-//   let filename = env::current_dir()?.join("arx.kdl");
-
-//   let contents = fs::read_to_string(filename)?;
-//   let doc: KdlDocument = contents.parse().expect("Failed to parse config file.");
-
-//   let replacements = config::get_replacements(&doc);
-//   let actions = config::get_actions(&doc);
-
-//   replacements.map(|items| {
-//     items.iter().for_each(|item| {
-//       let tag = &item.tag;
-//       let description = &item.description;
-
-//       println!("{tag} = {description}");
-//     })
-//   });
-
-//   actions.map(|action| {
-//     if let Action::Suite(suites) = action {
-//       let (resolved, unresolved) = config::resolve_requirements(&suites);
-
-//       println!("Resolved: {resolved:#?}");
-//       println!("Unresolved: {unresolved:#?}");
-//     }
-//   });
-
-//   Ok(())
-// }
